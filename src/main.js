ee4287dff3264b135c0e883be91a987a7df68a10
fxiong <1308694300@qq.com>
2019-04-24 19:13:39 +0800

    'ShopList和Login组件（静态）'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-/* eslint-disable no-new */
+import Header from './components/Header/Header'
+
+//注册全局组件
+
+Vue.component('Header',Header)
 new Vue({
   el: '#app',
   render: h => h(App),
